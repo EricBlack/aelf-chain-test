577021b9f16362b9aa85490a3adb383c9a9814e2
EricShu <blackeye286@126.com>
2019-06-07 22:15:56 +0800

    update api methods

--- api/Chain.go
+++ api/Chain.go
@@ -1,25 +1,28 @@
 package api
 
 import (
+	"encoding/json"
 	"ericshu.io/aelf-chain-test/dto"
 	"ericshu.io/aelf-chain-test/utils"
-	"encoding/json"
 )
 
-type Chain struct{
+type Chain struct {
 	HostUrl string
-	Client utils.HttpClient
+	Client  utils.HttpClient
 }
 
 func GetChain(hostUrl string) (chain Chain) {
-	return Chain {
+	chain = Chain{
 		HostUrl: hostUrl,
-		Client: utils.NewClient(hostUrl),
+		Client:  utils.NewClient(hostUrl),
 	}
+	chain.Client.SetDefaultHeaders()
+
+	return
 }
 
 func (chain Chain) GetBlockHeight() (result int64) {
-	response :=chain.Client.Get(GetBlockHeight, nil)
+	response := chain.Client.Get(GetBlockHeight, nil)
 	err := json.Unmarshal([]byte(response), &result)
 	if err != nil {
 		result = -1
@@ -28,11 +31,144 @@ func (chain Chain) GetBlockHeight() (result int64) {
 	return
 }
 
-func (chain Chain) GetChainInformation() (result dto.GetChainInformationOutput){
-	response :=chain.Client.Get(GetChainInformation, nil)
+func (chain Chain) GetChainStatus() (result dto.GetChainInformationOutput) {
+	response := chain.Client.Get(GetChainStatus, nil)
 	err := json.Unmarshal([]byte(response), &result)
 	if err != nil {
 		result = dto.GetChainInformationOutput{}
+	}
+
+	return
+}
+
+func (chain Chain) CreateRawTransaction(inputMessage dto.CreateRawTransactionInput) (result dto.CreateRawTransactionOutput) {
+	response := chain.Client.Post(CreateRawTransaction, nil)
+	err := json.Unmarshal([]byte(response), &result)
+	if err != nil {
+		result = dto.CreateRawTransactionOutput{}
+	}
+
+	return
+}
+
+func (chain Chain) BroadcastTransaction(inputMessage dto.BroadcastTransactionInput) (result dto.BroadcastTransactionOutput) {
+	response := chain.Client.Post(BroadcastTransaction, nil)
+	err := json.Unmarshal([]byte(response), &result)
+	if err != nil {
+		result = dto.BroadcastTransactionOutput{}
+	}
+
+	return
+}
+
+func (chain Chain) BroadcastTransactions(inputMessage dto.BroadcastTransactionsInput) (result []string) {
+	response := chain.Client.Post(BroadcastTransactions, nil)
+	err := json.Unmarshal([]byte(response), &result)
+	if err != nil {
+		result = []string{}
+	}
+
+	return
+}
+
+func (chain Chain) GetTransactionPoolStatus() (result dto.GetTransactionPoolStatusOutput) {
+	response := chain.Client.Get(GetTransactionPoolStatus, nil)
+	err := json.Unmarshal([]byte(response), &result)
+	if err != nil {
+		result = dto.GetTransactionPoolStatusOutput{}
+	}
+
+	return
+}
+
+func (chain Chain) GetBlockByHeight(height int64, includeTransactions bool) (result dto.BlockDto) {
+	params := map[string]interface{}{
+		"height":              height,
+		"includeTransactions": includeTransactions,
+	}
+
+	response := chain.Client.Get(GetBlockByHeight, params)
+	err := json.Unmarshal([]byte(response), &result)
+	if err != nil {
+		result = dto.BlockDto{}
+	}
+
+	return
+}
+
+func (chain Chain) GetBlockByHash(hash string, includeTransactions bool) (result dto.BlockDto) {
+	params := map[string]interface{}{
+		"blockHash":           hash,
+		"includeTransactions": includeTransactions,
+	}
+
+	response := chain.Client.Get(GetBlockByHash, params)
+	err := json.Unmarshal([]byte(response), &result)
+	if err != nil {
+		result = dto.BlockDto{}
+	}
+
+	return
+}
+
+func (chain Chain) GetBlockState(blockHash string) (result dto.BlockStateDto) {
+	params := map[string]interface{}{
+		"blockHash": blockHash,
+	}
+
+	response := chain.Client.Get(GetBlockState, params)
+	err := json.Unmarshal([]byte(response), &result)
+	if err != nil {
+		result = dto.BlockStateDto{}
+	}
+
+	return
+}
+
+func (chain Chain) Call(from, to string, inputMessage interface{}) (result interface{}) {
+	return
+}
+
+func (chain Chain) GetContractFileDescriptorSet(address string) (result []byte) {
+	params := map[string]interface{}{
+		"address": address,
+	}
+
+	response := chain.Client.Get(GetContractFileDescriptorSet, params)
+	err := json.Unmarshal([]byte(response), &result)
+	if err != nil {
+		result = []byte{}
+	}
+
+	result = []byte(response)
+	return
+}
+
+func (chain Chain) GetTransactionResult(transactionId string) (result dto.TransactionResultDto) {
+	params := map[string]interface{}{
+		"transactionId": transactionId,
+	}
+
+	response := chain.Client.Get(GetTransactionResult, params)
+	err := json.Unmarshal([]byte(response), &result)
+	if err != nil {
+		result = dto.TransactionResultDto{}
+	}
+
+	return
+}
+
+func (chain Chain) GetTransactionResults(blockHash string, offset, limit int32) (result []dto.TransactionResultDto) {
+	params := map[string]interface{}{
+		"blockHash": blockHash,
+		"offset": offset,
+		"limit": limit,
+	}
+
+	response := chain.Client.Get(GetTransactionResults, params)
+	err := json.Unmarshal([]byte(response), &result)
+	if err != nil {
+		result = []dto.TransactionResultDto{}
 	}
 
 	return
--- api/Net.go
+++ api/Net.go
@@ -1,1 +1,58 @@
 package api
+
+import (
+	"encoding/json"
+	"ericshu.io/aelf-chain-test/dto"
+	"ericshu.io/aelf-chain-test/utils"
+)
+
+type Net struct {
+	HostUrl string
+	Client  utils.HttpClient
+}
+
+func GetNet(hostUrl string) (net Net) {
+	net = Net{
+		HostUrl: hostUrl,
+		Client:  utils.NewClient(hostUrl),
+	}
+	net.Client.SetDefaultHeaders()
+
+	return
+}
+
+func (net Net) GetPeers() (result []dto.PeerDto) {
+	response := net.Client.Get(GetPeers, nil)
+	err := json.Unmarshal([]byte(response), &result)
+	if err != nil {
+		result = []dto.PeerDto{}
+	}
+
+	return
+}
+
+func (net Net) AddPeer(address string) (result bool) {
+	params := map[string]interface{}{
+		"address": address,
+	}
+	response := net.Client.Post(RemovePeer, params)
+	err := json.Unmarshal([]byte(response), &result)
+	if err != nil {
+		result = false
+	}
+
+	return
+}
+
+func (net Net) RemovePeer(address string) (result bool) {
+	params := map[string]interface{}{
+		"address": address,
+	}
+	response := net.Client.Delete(RemovePeer, params)
+	err := json.Unmarshal([]byte(response), &result)
+	if err != nil {
+		result = false
+	}
+
+	return
+}
